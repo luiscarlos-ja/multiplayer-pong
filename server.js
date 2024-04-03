6795e68a7d8e492ff44c4e9149f18bd0e7a65c54
Luis Carlos <luiscarlos_ja@outlook.com>
2024-04-02 19:12:17 -0600

    Refactor code for game initialization and player synchronization

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 const server = require("http").createServer();
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 const PORT = process.env.PORT || 3000;
 
-io.on("connection", (socket) => {
-  console.log("Client connected");
+let readyPlayerCount = 0;
 
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
+io.on("connection", (socket) => {
+  console.log("Client connected", socket.id);
+
+  socket.on("disconnect", (reason) => {
+    console.log(`Client disconnected ${socket.id}: ${reason}`);
   });
 
-  socket.on("message", (message) => {
-    console.log(`Message received: ${message}`);
-    socket.broadcast.emit("message", message);
+  socket.on("ready", () => {
+    console.log(`Player Ready! ${socket.id}`);
+    readyPlayerCount++;
+
+    if (readyPlayerCount === 2) {
+      io.emit("startGame", socket.id);
+    }
+  });
+
+  socket.on("paddleMove", (paddleData) => {
+    socket.broadcast.emit("paddleMove", paddleData);
+  });
+
+  socket.on("ballMove", (ballData) => {
+    socket.broadcast.emit("ballMove", ballData);
   });
 });
 
